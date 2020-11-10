7d54a2db170f1c03e4a3760a7372392d72daeecf
sasataniryota <61574281+sasataniryota@users.noreply.github.com>
2020-11-10 10:43:57 +0900

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Amplify, API, graphqlOperation } from 'aws-amplify'
+import * as gqlQueries from './graphql/queries' // read
+import * as gqlMutations from './graphql/mutations' // create, update, delete
 
-export default function App() {
+const pretapoConfig = {
+  "aws_project_region": "ap-northeast-1",
+  "aws_appsync_graphqlEndpoint": "https://lpysywb5rnamtdmuojjypxxkri.appsync-api.ap-northeast-1.amazonaws.com/graphql",
+  "aws_appsync_region": "ap-northeast-1",
+  "aws_appsync_authenticationType": "API_KEY",
+  "aws_appsync_apiKey": "da2-xgoywjersjefvh6kg5fgy3r6ci",
+}
+
+Amplify.configure(pretapoConfig)
+
+export default  App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect( async () => {
+    const user = await API.graphql(graphqlOperation(gqlQueries.getUser, { id: 'ryotadegozaru917@icloud.com' }))
+    console.log(user)
+    setState(user)
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>テスト</Text>
+      <Text>{data}</Text>
       <StatusBar style="auto" />
     </View>
   );
