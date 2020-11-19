b975231a4a3c87471a3fbecc10214f1007b482ae
wafuwafu13 <mariobaske@i.softbank.jp>
2020-11-19 19:51:10 +0900

    インデックスを取る

--- screens/ImageTab/ItemImageList.js
+++ screens/ImageTab/ItemImageList.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect} from 'react'
 import {Dimensions, Image, View, SafeAreaView, Button} from 'react-native'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen'
 import AutoDragSortableView from '../../widget/AutoDragSortableView'
@@ -11,7 +11,16 @@ const childrenHeight = 48*4
 
 const ItemImageList = ({navigation}) => {
 
-    const [imageURLs, setImageURLs] = useState(navigation.state.params.item.imageURLs);
+    const imageURLs = navigation.state.params.item.imageURLs;
+    // 1からはじまる
+    const imageURLsIndex = {};
+
+    useEffect(() => {
+        imageURLs.forEach((imageURL, i) => {
+            imageURLsIndex[i+1] = imageURL
+        })
+        console.log(imageURLsIndex)
+    }, [])
 
     const renderItem = (imageURLs,index) => {
         return (
@@ -37,6 +46,28 @@ const ItemImageList = ({navigation}) => {
                 keyExtractor={(imageURLs,index)=> imageURLs}
                 renderItem={(imageURLs,index)=>{
                     return renderItem(imageURLs,index)
+                }}
+                onDragEnd={(from, to)=> {
+                    const copedImageURLsIndex = { ...imageURLsIndex }
+                    const finalIndex = imageURLs.length - 1
+                    // 最小から最大へ動いた時
+                    if (from == 0 && to == finalIndex) {
+                        imageURLsIndex[finalIndex+1] = copedImageURLsIndex[1]
+                        for (let i = 1; i <= finalIndex; i++) {
+                            imageURLsIndex[i] = copedImageURLsIndex[i+1]
+                        }
+                    // 最大から最小へ動いた時
+                    } else if (from == finalIndex && to == 0) {
+                        imageURLsIndex[1] = copedImageURLsIndex[finalIndex+1]
+                        for (let i = 2; i <= finalIndex + 1; i++) {
+                            imageURLsIndex[i] = copedImageURLsIndex[i-1]
+                        }
+                    } else {
+                        imageURLsIndex[from+1] = copedImageURLsIndex[to+1]
+                        imageURLsIndex[to+1] = copedImageURLsIndex[from+1]
+                    }
+                    console.log(from, to)
+                    console.log(imageURLsIndex)
                 }}
             />
             <Button
