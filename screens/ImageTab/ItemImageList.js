7a03b805027b96441fafbf4923942634a8094f7c
wafuwafu13 <mariobaske@i.softbank.jp>
2020-11-19 17:59:14 +0900

    D&D

--- screens/ImageTab/ItemImageList.js
+++ screens/ImageTab/ItemImageList.js
@@ -1,16 +1,46 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native'
-import { API, graphqlOperation } from 'aws-amplify'
-import * as gqlQueries from '../../graphql/queries' // read
+import React, {useState} from 'react'
+import {Dimensions, Image, View, SafeAreaView} from 'react-native'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen'
-import Icon from 'react-native-vector-icons/Feather'
+import AutoDragSortableView from '../../widget/AutoDragSortableView'
 
-const ItemImageList = () => {
+const {width} = Dimensions.get('window')
+
+const parentWidth = width
+const childrenWidth = width/3 - 20
+const childrenHeight = 48*4
+
+const ItemImageList = ({navigation}) => {
+
+    const [imageURLs, setImageURLs] = useState(navigation.state.params.item.imageURLs);
+
+    const renderItem = (imageURLs,index) => {
+        return (
+            <View>
+                <Image source={{uri: imageURLs}} style={{ width: wp('33%'), height: wp('33%') }} />
+            </View>
+        )
+    }
+
     return (
-        <View>
-            <Text>hello</Text>
-        </View>
+        <SafeAreaView style={{flex: 1}}>
+            <AutoDragSortableView
+                dataSource={imageURLs}
+                
+                parentWidth={parentWidth}
+                childrenWidth= {childrenWidth}
+                marginChildrenBottom={10}
+                marginChildrenRight={10}
+                marginChildrenLeft = {10}
+                marginChildrenTop = {10}
+                childrenHeight={childrenHeight}
+
+                keyExtractor={(imageURLs,index)=> imageURLs}
+                renderItem={(imageURLs,index)=>{
+                    return renderItem(imageURLs,index)
+                }}
+            />
+        </SafeAreaView>
     )
 }
 
-export default ItemImageList
+export default ItemImageList;
